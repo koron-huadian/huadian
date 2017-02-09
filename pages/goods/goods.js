b5b47b32d1f3ea90f9351d92e396e78edeefbb13
ayjacket <ayjacket@icloud.com>
2017-02-09 18:02:35 +0800

    Update goods.js

--- pages/goods/goods.js
+++ pages/goods/goods.js
@@ -1079,7 +1079,7 @@ Page({
         ],
         toView: '0',
         scrollTop: 100,
-        foodCounts: ''
+        foodCounts: 1
     },
     selectFoods: function (event) {
         var food = event.currentTarget.dataset.food;
@@ -1105,19 +1105,22 @@ Page({
     decreaseCart: function (e) {
         var foodCount = e.currentTarget.dataset.food.count;
         this.setData({
-            foodCounts: foodCount--
+            foodCounts: foodCount - 1
         })
     },
-    addCart(event) {
-        if (!this.data.foodCounts) {
-            this.setData({
-                foodCounts: 1
-            })
-        } else {
-            this.setData({
-                foodCounts: foodCounts++
-            })
-        }
+    addCart(e) {
+        // var foodCount = e.currentTarget.dataset.food.count;
+        console.log(e);
+        // if (!this.data.foodCounts) {
+        //     this.setData({
+        //         foodCounts: 1
+        //     })
+        // } else {
+        //     this.setData({
+        //         foodCounts: foodCount + 1
+        //     })
+        //     console.log(foodCount)
+        // }
     },
     onLoad: function (options) {
         // 页面初始化 options为页面跳转所带来的参数
